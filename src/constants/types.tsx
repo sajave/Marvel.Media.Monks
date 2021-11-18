fd6272894229d022231a88091f9f759773380482
Santiago <santiagojavierveiga@gmail.com>
2021-11-18 01:14:48 -0300

    feat(client/types): create types and interfaces

---
+++ src/constants/types.tsx
@@ -0,0 +1,81 @@
+export interface action {
+  type: string;
+  payload: any;
+}
+
+export interface rootState {
+  characters: Character[] | undefined;
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  description: string;
+  modified: string;
+  resourceURI: string;
+  urls: Url[];
+  thumbnail: Image;
+  comics: ComicList;
+  stories: StoryList;
+  events: EventList;
+  series: SeriesList;
+}
+
+export interface Url {
+  type: string;
+  url: string;
+}
+
+export interface Image {
+  path: string;
+  extension: string;
+}
+
+export interface ComicList {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: ComicSummary[];
+}
+
+export interface ComicSummary {
+  resourceURI: string;
+  name: string;
+}
+
+export interface StoryList {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: StorySummary[];
+}
+
+export interface StorySummary {
+  resourceURI: string;
+  name: string;
+  type: string;
+}
+
+export interface EventList {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: EventSummary[];
+}
+
+export interface EventSummary {
+  resourceURI: string;
+  name: string;
+}
+
+export interface SeriesList {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: SeriesSummary[];
+}
+
+export interface SeriesSummary {
+  resourceURI: string;
+  name: string;
+}
