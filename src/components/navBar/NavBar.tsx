3ea3144a697aa934bc8a10b8927a07ccd0ce64f9
Santiago <santiagojavierveiga@gmail.com>
2021-11-19 00:02:20 -0300

    feat(client/NavBar): NavBar shows BACK to home button

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,13 +1,23 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
+import {useHistory, useParams} from "react-router";
 import {getAllCharacters} from "../../actions";
 import {rootState} from "../../constants/types";
 import "./NavBarStyle.css";
 
 export function NavBar() {
+  const history = useHistory();
+  const {id} = useParams<{id?: string}>();
+
   return (
     <header className='navbar'>
-      <div className='Title'>CHARACTERS</div>
+      {id ? (
+        <div className='Title' onClick={() => history.push("/")}>
+          BACK
+        </div>
+      ) : (
+        <div className='Title'>CHARACTERS</div>
+      )}
       <img
         src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg'
         alt='MARVEL'
