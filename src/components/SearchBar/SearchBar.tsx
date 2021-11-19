28836b8f2170f3aa631f1bc576c2e2d4a35db5a6
Santiago <santiagojavierveiga@gmail.com>
2021-11-18 21:57:12 -0300

    feat(client/SearchBar): added styles

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,26 +1,24 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {getAllCharacters} from "../../actions";
 import {rootState} from "../../constants/types";
+import {SearchBarIcon} from "../searchIcon/SearchIcon";
 import "./SearchBarStyle.css";
 
 export function SearchBar() {
   const allCharacters = useSelector((state: rootState) => state.characters);
+  const [filter, setFilter] = useState("name");
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        /* onSearch(search); */
-      }}
-    >
+    <header className='headerSearch'>
       <input
         type='text'
         placeholder='Search...'
+        className='inputSearch'
         /* value={search}
         onChange={(e) => set(e.target.value)} */
       />
-      <input type='submit' value='Agregar' />
-    </form>
+      <SearchBarIcon />
+    </header>
   );
 }
