331ce57e9c055b01e9a9d3a18b3489d8062c4343
Santiago <santiagojavierveiga@gmail.com>
2021-11-18 21:54:01 -0300

    feat(client/App): added router to character detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Route} from "react-router-dom";
 import "./App.css";
+import {CharacterDetail} from "./components/characterDetail/CharacterDetail";
 import {Home} from "./components/home/Home";
 import {NavBar} from "./components/navBar/NavBar";
 import {SearchBar} from "./components/SearchBar/SearchBar";
@@ -8,9 +9,9 @@ import {SearchBar} from "./components/SearchBar/SearchBar";
 function App() {
   return (
     <div className='App'>
-      <Route exact path='/' component={NavBar} />
-      <Route exact path='/' component={SearchBar} />
+      <Route path='/' component={NavBar} />
       <Route exact path='/' component={Home} />
+      <Route exact path='/:id' component={CharacterDetail} />
     </div>
   );
 }
