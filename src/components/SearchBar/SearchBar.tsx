9c68098909a7044e42b92de07578e050adae50f8
Santiago <santiagojavierveiga@gmail.com>
2021-11-20 16:49:16 -0300

    feat(client/SearchBar): added logic to set selected and unselected theme

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,6 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
+import {rootState} from "../../constants/types";
 import {FilterBtn} from "../filterBtn/FilterBtn";
 import {SearchBarIcon} from "../searchIcon/SearchIcon";
 import "./SearchBarStyle.css";
@@ -8,8 +10,32 @@ type Props = {
 };
 
 export function SearchBar({onSearch}: Props) {
+  const filterSearchBy = useSelector(
+    (state: rootState) => state.filterSearchBy
+  );
   const [searchInput, setSearchInput] = useState<string>("");
+
+  const filters = ["name", "comics", "stories", "series"];
+  const [colors, setColors] = useState<string[]>([
+    "selected",
+    "unSelected",
+    "unSelected",
+    "unSelected",
+  ]);
+
   onSearch(searchInput);
+
+  useEffect(() => {
+    let filterColors: string[] = [];
+    for (let i = 0; i < filters.length; i++) {
+      if (filters[i] === filterSearchBy) {
+        filterColors = [...filterColors, "selected"];
+      } else {
+        filterColors = [...filterColors, "unSelected"];
+      }
+    }
+    setColors(filterColors);
+  }, [filterSearchBy]);
 
   return (
     <header className='headerSearch'>
@@ -20,11 +46,20 @@ export function SearchBar({onSearch}: Props) {
         value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
       />
-
-      <FilterBtn id='name' />
-      <FilterBtn id='comics' />
-      <FilterBtn id='stories' />
-      <FilterBtn id='series' />
+      <div className='groupBtn'>
+        <div>
+          <FilterBtn id='name' styleTheme={colors[0]} />
+        </div>
+        <div>
+          <FilterBtn id='comics' styleTheme={colors[1]} />
+        </div>
+        <div>
+          <FilterBtn id='stories' styleTheme={colors[2]} />
+        </div>
+        <div>
+          <FilterBtn id='series' styleTheme={colors[3]} />
+        </div>
+      </div>
     </header>
   );
 }
