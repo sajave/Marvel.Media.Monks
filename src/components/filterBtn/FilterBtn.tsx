43ea1cbc75b380fb1ef3e29297a83fe3c9973954
Santiago <santiagojavierveiga@gmail.com>
2021-11-20 16:49:45 -0300

    fix(client/FilterBtn): fixed styles

--- src/components/filterBtn/FilterBtn.tsx
+++ src/components/filterBtn/FilterBtn.tsx
@@ -3,18 +3,21 @@ import {useDispatch, useSelector} from "react-redux";
 import {getAllCharacters, setFilterSearchBy} from "../../actions";
 import {rootState} from "../../constants/types";
 import {SearchBarIcon} from "../searchIcon/SearchIcon";
+import "./filterBtn.css";
 
 type Props = {
   id: string;
+  styleTheme: string;
 };
 
-export function FilterBtn({id}: Props) {
+export function FilterBtn({id, styleTheme}: Props) {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <div className='btnContainer'>
       <button
         id={id}
+        className={styleTheme}
         onClick={(e: any) => {
           dispatch(setFilterSearchBy(e.target.id));
         }}
