33a87677ee8cd9aeab34a5aedf1b314084bef88b
Santiago <santiagojavierveiga@gmail.com>
2021-11-21 17:22:16 -0300

    feat(client/types): added types

--- src/constants/types.tsx
+++ src/constants/types.tsx
@@ -5,9 +5,11 @@ export interface action {
 
 export interface rootState {
   characters: Character[] | undefined;
+  comicsById: Comic[] | undefined;
   filterSearchBy: string | undefined;
 }
 
+/* Character types */
 export interface Character {
   id: number;
   name: string;
@@ -26,57 +28,124 @@ export interface Url {
   type: string;
   url: string;
 }
-
 export interface Image {
   path: string;
   extension: string;
 }
-
 export interface ComicList {
   available: number;
   returned: number;
   collectionURI: string;
   items: ComicSummary[];
 }
-
 export interface ComicSummary {
   resourceURI: string;
   name: string;
 }
-
 export interface StoryList {
   available: number;
   returned: number;
   collectionURI: string;
   items: StorySummary[];
 }
-
 export interface StorySummary {
   resourceURI: string;
   name: string;
   type: string;
 }
-
 export interface EventList {
   available: number;
   returned: number;
   collectionURI: string;
   items: EventSummary[];
 }
-
 export interface EventSummary {
   resourceURI: string;
   name: string;
 }
-
 export interface SeriesList {
   available: number;
   returned: number;
   collectionURI: string;
   items: SeriesSummary[];
 }
-
 export interface SeriesSummary {
   resourceURI: string;
   name: string;
 }
+/* Character types */
+
+/* Comic types */
+export interface Comic {
+  id: number;
+  digitalId: number;
+  title: string;
+  issueNumber: null;
+  variantDescription: string;
+  description: string;
+  modified: Date;
+  isbn: string;
+  upc: string;
+  diamondCode: string;
+  ean: string;
+  issn: string;
+  format: string;
+  pageCount: number;
+  textObjects: TextObject[];
+  resourceURI: string;
+  urls: Url[];
+  series: SeriesSummary;
+  variants: ComicSummary[];
+  collections: ComicSummary[];
+  collectedIssues: ComicSummary[];
+  dates: ComicDate[];
+  price: ComicPrice[];
+  thumbnail: Image;
+  images: Image[];
+  creators: CreatorList;
+  character: CharacterList;
+  stories: StoryList;
+  events: EventList;
+}
+
+export interface TextObject {
+  type: string;
+  language: string;
+  text: string;
+}
+export interface Url {
+  type: string;
+  url: string;
+}
+export interface ComicDate {
+  type: string;
+  date: Date;
+}
+export interface ComicPrice {
+  type: string;
+  price: number;
+}
+export interface CreatorList {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: CreatorSummary[];
+}
+export interface CreatorSummary {
+  resourceURI: string;
+  name: string;
+  role: string;
+}
+export interface CharacterList {
+  available: number;
+  returned: number;
+  collectionURI: string;
+  items: CharacterSummary[];
+}
+export interface CharacterSummary {
+  resourceURI: string;
+  name: string;
+  role: string;
+}
+
+/* Comic types */
