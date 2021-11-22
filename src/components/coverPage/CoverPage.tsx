2fbbb8a3c11cc95094a165468ffc0a5bc9ef6204
Santiago <santiagojavierveiga@gmail.com>
2021-11-22 12:27:16 -0300

    fix(client/CoverPage): modified styles

--- src/components/coverPage/CoverPage.tsx
+++ src/components/coverPage/CoverPage.tsx
@@ -3,12 +3,19 @@ import "./CoverPage.css";
 type Props = {
   title: string;
   thumbnail: string | undefined;
+  classNameContainer: string;
+  classNameImg: string;
 };
 
-export const CoverPage = ({title, thumbnail}: Props) => {
+export const CoverPage = ({
+  title,
+  thumbnail,
+  classNameContainer,
+  classNameImg,
+}: Props) => {
   return (
-    <div className='coverContainer'>
-      <div>
+    <div className={classNameContainer}>
+      <div className={classNameImg}>
         <img src={thumbnail} alt='Not available' className='coverImg' />
       </div>
       <div className='titleCover'>{title}</div>
