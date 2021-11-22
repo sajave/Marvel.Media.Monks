e7be3741d6b44d91b0047d85611f039bcc7e30d0
Santiago <santiagojavierveiga@gmail.com>
2021-11-21 22:01:03 -0300

    feat(client/filterFunction): added filter by stories and series

--- src/utils/filterFunction.tsx
+++ src/utils/filterFunction.tsx
@@ -1,26 +1,50 @@
 export const filterFunction = (
   allCharacters: any,
   filterSearchBy: any,
-  setAux: any,
+  setFilteredSearch: any,
   search: any
 ) => {
   if (filterSearchBy === "name") {
     const aux = allCharacters?.filter((e: any) => {
       return e.name.includes(search);
     });
-    setAux(aux);
+    setFilteredSearch(aux);
     return;
   }
 
   if (filterSearchBy === "comics") {
-    const characters = allCharacters?.filter((e: any) => {
-      let aux = e.comics.items.some((comic: any) =>
+    const characters = allCharacters?.filter((character: any) => {
+      let aux = character.comics.items.some((comic: any) =>
         comic.name.includes(search)
       );
       if (aux) {
-        return e;
+        return character;
       }
     });
-    setAux(characters);
+    setFilteredSearch(characters);
+  }
+
+  if (filterSearchBy === "stories") {
+    const characters = allCharacters?.filter((character: any) => {
+      let aux = character.stories.items.some((story: any) =>
+        story.name.includes(search)
+      );
+      if (aux) {
+        return character;
+      }
+    });
+    setFilteredSearch(characters);
+  }
+
+  if (filterSearchBy === "series") {
+    const characters = allCharacters?.filter((character: any) => {
+      let aux = character.stories.items.some((story: any) =>
+        story.name.includes(search)
+      );
+      if (aux) {
+        return character;
+      }
+    });
+    setFilteredSearch(characters);
   }
 };
